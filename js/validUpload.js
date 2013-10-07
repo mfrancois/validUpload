4c0b9b188a9d5142839cadfccf96adaff2a929b3
Maxime François <mfrancois@tequilarapido.com>
2013-10-07 17:10:54 +0200

    Install bower package
    Add bower component
    Add twitter, jquery, validation-engine, plupload
    Add index for starting component

--- js/validUpload.js
+++ js/validUpload.js
@@ -1,128 +1,149 @@
-(function ($) {
+(function (jQuery) {
 
-    $.validUpload = function (e, o) {
-        this.settings = $.extend(true, {}, $.validUpload.defaults, o);
+    jQuery.validUpload = function (e, o) {
+        this.settings = jQuery.extend(true, {}, jQuery.validUpload.defaults, o);
         this.element = e;
         this.init();
     }
 
-    var $validUpload = $.validUpload;
-    $.extend($.validUpload, {
-        defaults:{
-            browse_button:'pickfiles',
-            container:'container',
-            selectorInput:'.elementUploaded',
-            classEmpty:'empty',
-            classUplaoded:'uploaded',
-            selectorUploadDelete:'.upload-delete',
-            selectorProgress:'.progressUploader',
-            selectorInjection:'.filelist',
-            filters:[
+    var jQueryvalidUpload = jQuery.validUpload;
+    jQuery.extend(jQuery.validUpload, {
+        defaults: {
+            browse_button: 'browse',
+            selectorInput: '.elementUploaded',
+            classEmpty: 'empty',
+            classUplaoded: 'uploaded',
+            selectorUploadDelete: '.upload-delete',
+            selectorUploadFinish: '.finished',
+            selectorProgress: '.progress-bar',
+            selectorProgressContener: '.progressUploader',
+            selectorInjection: '.filelist',
+            filters: [
             ],
-            dataUploader:{
-                runtimes:'html5,flash,html4',
-                url:'/upload/upload.php',
-                flash_swf_url:'/js/uploader/plupload.flash.swf',
-                chunk_size:'1mb',
-                multi_selection:false,
-                max_file_count:1,
-                unique_names:true
+            dataUploader: {
+                runtimes: 'html5,flash,html4',
+                url: 'upload/upload.php',
+                flash_swf_url: '/js/uploader/plupload.flash.swf',
+                chunk_size: '1mb',
+                multi_selection: false,
+                max_file_count: 1,
+                unique_names: true
             }
         },
-        prototype:{
-            init:function () {
+        prototype: {
+            init: function () {
                 this.timeout = '';
                 this.initData();
+                this.initDisplay();
                 this.initEvent();
 
                 this.elementUploader.init();
             },
 
-            initData:function () {
+            initData: function () {
                 var data = this.settings.dataUploader;
 
                 data.browse_button = this.settings.browse_button;
-                data.container = this.settings.container;
+                data.container = this.element.id;
                 data.filters = this.settings.filters;
 
                 this.elementUploader = new plupload.Uploader(data);
 
             },
-            initEvent:function () {
+
+            initDisplay: function () {
+                jQuery(this.settings.selectorProgressContener, this.element).hide();
+                this.tooglRemove();
+                this.toogleFinish();
+            },
+
+            tooglRemove: function () {
+                var valInput = jQuery(this.settings.selectorInput, this.element).val();
+                if (valInput == '') {
+                    jQuery(this.settings.selectorUploadDelete, this.element).hide();
+                } else {
+                    jQuery(this.settings.selectorUploadDelete, this.element).show();
+                }
+
+            },
+            toogleFinish: function () {
+                var valInput = jQuery(this.settings.selectorInput, this.element).val();
+                if (valInput == '') {
+                    jQuery(this.settings.selectorUploadFinish, this.element).hide();
+                } else {
+                    jQuery(this.settings.selectorUploadFinish, this.element).show();
+                }
+            },
+
+            initEvent: function () {
                 this.elementUploader.unbind('FilesAdded');
-                this.elementUploader.bind('FilesAdded', $.proxy(this.addFile, this));
+                this.elementUploader.bind('FilesAdded', jQuery.proxy(this.addFile, this));
 
                 this.elementUploader.unbind('UploadProgress');
-                this.elementUploader.bind('UploadProgress', $.proxy(this.progresse, this));
+                this.elementUploader.bind('UploadProgress', jQuery.proxy(this.progresse, this));
 
                 this.elementUploader.unbind('Init');
-                this.elementUploader.bind('Init', $.proxy(this.initUploader, this));
+                this.elementUploader.bind('Init', jQuery.proxy(this.initUploader, this));
 
                 this.elementUploader.unbind('FileUploaded');
-                this.elementUploader.bind('FileUploaded', $.proxy(this.endUpload, this));
+                this.elementUploader.bind('FileUploaded', jQuery.proxy(this.endUpload, this));
 
                 this.elementUploader.unbind('Error');
-                this.elementUploader.bind('Error', $.proxy(this.errorUpload, this));
+                this.elementUploader.bind('Error', jQuery.proxy(this.errorUpload, this));
 
-                $(this.settings.selectorUploadDelete,this.element).click($.proxy(this.removeElement, this))
+                jQuery(this.settings.selectorUploadDelete, this.element).click(jQuery.proxy(this.removeElement, this))
 
             },
 
-            errorUpload: function(up, err){
-                if(this.timeout != ''){
+            errorUpload: function (up, err) {
+                if (this.timeout != '') {
                     clearTimeout(this.timeout);
                 }
-                $(this.element).validationEngine('showPrompt',err.message);
-                $(this.settings.selectorProgress, this.element).hide('slow');
+                jQuery(this.element).validationEngine('showPrompt', err.message);
+                jQuery(this.settings.selectorProgressContener, this.element).hide();
+                console.log(err);
             },
 
-            endUpload: function(up, file, info){
-                $(this.settings.selectorInput,this.element).val(file.target_name);
-                $(this.settings.selectorInjection,this.element).html(file.target_name);
-                $(this.settings.selectorProgress, this.element).hide('slow');
-                $(this.element).removeClass(this.settings.classEmpty).addClass(this.settings.classUplaoded);
-                $(this.settings.selectorInjection,this.element).removeClass(this.settings.classEmpty).addClass(this.settings.classUplaoded);
+            endUpload: function (up, file, info) {
+                jQuery(this.settings.selectorInput, this.element).val(file.target_name);
+                jQuery(this.settings.selectorInjection, this.element).html(file.target_name);
+                this.initDisplay();
             },
 
-            removeElement:function(e){
+            removeElement: function (e) {
                 e.preventDefault();
-                $(this.element).removeClass(this.settings.classUplaoded).addClass(this.settings.classEmpty);
-                $(this.settings.selectorInjection,this.element).html('');
-                $(this.settings.selectorInjection,this.element).removeClass(this.settings.classUplaoded).addClass(this.settings.classEmpty);
-                $(this.settings.selectorInput,this.element).val('');
+                jQuery(this.settings.selectorInjection, this.element).html('');
+                jQuery(this.settings.selectorInput, this.element).val('');
+                this.initDisplay();
 
                 return false;
             },
 
-            initUploader:function (up, params) {
+            initUploader: function (up, params) {
             },
 
-            addFile:function (up, files) {
-                $(this.element).removeClass(this.settings.classUplaoded).addClass(this.settings.classEmpty);
-                $(this.settings.selectorInjection,this.element).removeClass(this.settings.classUplaoded).addClass(this.settings.classEmpty);
-                $(this.element).validationEngine('hidePrompt');
-                this.timeout = setTimeout($.proxy(this.startUpload, this),200);
+            addFile: function (up, files) {
+                jQuery(this.element).validationEngine('hide');
+                this.timeout = setTimeout(jQuery.proxy(this.startUpload, this), 200);
             },
 
-            startUpload:function(){
-                $(this.settings.selectorProgress, this.element).show('slow');
+            startUpload: function () {
+                jQuery(this.settings.selectorProgressContener, this.element).show();
                 this.elementUploader.start();
             },
-            progresse:function (up, file) {
-                $(this.settings.selectorProgress, this.element).progressbar({
-                    value:file.percent
+            progresse: function (up, file) {
+                jQuery(this.settings.selectorProgress, this.element).css({
+                    width: file.percent + "%"
                 });
+
             }
         }
     });
 
-    $.fn.validUpload = function(options)
-    {
-        return this.each(function()
-        {
-            if (undefined == $(this).data('validUpload'))
-            {
-                $(this).data('validUpload', new $.validUpload(this, options));
+    jQuery.fn.validUpload = function (options) {
+        return this.each(function () {
+            if (undefined == jQuery(this).data('validUpload')) {
+                jQuery(this).data('validUpload', new jQuery.validUpload(this, options));
             }
         });
     };
